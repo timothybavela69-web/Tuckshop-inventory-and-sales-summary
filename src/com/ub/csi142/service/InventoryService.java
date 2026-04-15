97954870aa9f3123993c28a2622cf40ae705b5fc
Emmanuel-Tshabadira <tshabadira1997@gmail.com>
2026-04-15 20:25:07 +0200

    Add basic methods to InventoryService

--- src/com/ub/csi142/service/InventoryService.java
+++ src/com/ub/csi142/service/InventoryService.java
@@ -1,4 +1,15 @@
 package com.ub.csi142.service;
 
 public class InventoryService {
+
+   public InventoryService() {
+    }
+
+    public void addProduct() {
+        System.out.println("Product added");
+    }
+
+    public void viewProducts() {
+        System.out.println("Viewing products");
+    }
 }

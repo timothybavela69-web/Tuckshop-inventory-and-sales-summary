a2cb54a87718961630c7936b0d059d6896229afe
mooketsaemely-cmyk <mooketsaemely@gmail.com>
2026-04-15 15:01:37 +0200

    Add abstract Product class with fileds
    Removed extra whitespace in the Product class.

--- src/com/ub/csi142/model/product.java
+++ src/com/ub/csi142/model/product.java
@@ -1,15 +1,13 @@
 package model;
 public abstract class Product {
-  protected String name ;
-  protected double price ;
-  protected int quantity  ;
+  protected String name;
+  protected double price;
+  protected int quantity;
 
   public Product(String name,double price,int quantity){
   this.name=name; 
   this.price=price;
   this.quantity=quantity;
-
- 
   }
 }
   

782c5abbd2166556c245792ab5395bb9c3d34789
mooketsaemely-cmyk <mooketsaemely@gmail.com>
2026-04-15 14:56:35 +0200

    Establish abstract Product class with basic properties

---
+++ src/com/ub/csi142/model/product.java
@@ -0,0 +1,15 @@
+package model;
+public abstract class Product {
+  protected String name ;
+  protected double price ;
+  protected int quantity  ;
+
+  public Product(String name,double price,int quantity){
+  this.name=name;
+  this.price=price;
+  this.quantity=quantity;
+
+ 
+  }
+}
+  

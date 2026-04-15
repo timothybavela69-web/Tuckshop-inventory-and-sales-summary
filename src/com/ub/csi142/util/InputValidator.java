1f0c2d7f1025be0f275f004f93793e3dd3d5f9f3
Timothy Bavela <timothybavela69@gmail.com>
2026-04-15 21:34:23 +0200

    Add menu choice validation method

--- src/com/ub/csi142/util/InputValidator.java
+++ src/com/ub/csi142/util/InputValidator.java
@@ -3,4 +3,7 @@ public class InputValidator{
   public static boolean isPositive(int value){
     return value >0;
   }
+  public static boolean isValidChoice(int choice){
+    return choice >= 1 && choice<= 5;
+  }
 }

e7b9bdea5309053e58e19de6e6ac6848c5695dd2
masediselinda0-crypto <masediselinda0@gmail.com>
2026-04-15 07:51:53 -0700

    Create SalesServices.java

---
+++ src/com/ub/csi142/service/SalesServices.java
@@ -0,0 +1,11 @@
+package com.ub.csi142.service;
+public class SalesServices{
+  public SalesServices() {
+  }
+  public void recordSale() {
+    System.out.printIn("sales recorded");
+      }
+  public void viewSalesReport() {
+    System.out.printIn("viewing sales report");
+  }
+}

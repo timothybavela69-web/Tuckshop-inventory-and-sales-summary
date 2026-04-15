626d7f07ecc054ebf725e7b980ba83f8dc3db189
masediselinda0-crypto <masediselinda0@gmail.com>
2026-04-15 07:54:12 -0700

    Update InventoryService.java

--- src/com/ub/csi142/service/InventoryService.java
+++ src/com/ub/csi142/service/InventoryService.java
@@ -1,10 +1,11 @@
-package com.ub.csi142.services,
+package com.ub.csi142.services;
 public class InventoryService{
-  public IventoryService{ 
+  public IventoryService() {
   }
   public void addproduct() {
-    System.out.printIn("product added"),
+    System.out.printIn("product added");
       }
   public void viewproducts() {
-    System.out.printIn("viewing products"),
+    System.out.printIn("viewing products");
       }
+}

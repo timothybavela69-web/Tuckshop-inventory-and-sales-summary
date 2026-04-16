d34f44e3fff0638d8a8709a01c94b70538f1ccaa
Emely Mooketsa <mooketsaemely@gmail.com>
2026-04-16 14:24:22 +0200

    Add Sales class

---
+++ src/com/ub/csi142/model/Sales.java
@@ -0,0 +1,12 @@
+ package com.ub.csi142.model;
+
+public class Sales{
+  private String productName;
+  private int quantity;
+  private double totalPrice;
+
+  public Sale (String productName,int quantity,double totalPrice){
+  this.productName=productName;
+  this.totalPrice=totalPrice;
+  }
+  }  
